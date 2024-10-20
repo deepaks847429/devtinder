9e3bc04ce9c893294d0ba4274c24999bffaecf86
ambitious_hustler <deepaks9631342845@gmail.com>
2024-10-20 21:27:00 +0530

    made signup api dynamic

--- src/app.js
+++ src/app.js
@@ -3,15 +3,16 @@ const connectDB=require("./config/database");
 const app=express();
 const User=require("./models/user")
 
+app.use(express.json());
+
 app.post("/signup", async(req, res)=>{
-  const user = new User({
-    firstName: "Deepak",
-    lastName: "Kumar",
-    emailid: "deep.dkt0@gmail.com",
-    password:"deepak@123"
-  });
-  await user.save();
+  const user = new User(req.body);
+  try {
+    await user.save();
   res.send("User added successfully!")
+  } catch (err) {
+    res.status(400).send("Error saving the user:"+ err.message);
+  }
 });
 
 connectDB()
