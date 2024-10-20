ca7542afb4bb487359b11ea0bacc6e330fb73f43
ambitious_hustler <deepaks9631342845@gmail.com>
2024-10-20 14:59:40 +0530

    basic fundamentals on routing and regex

---
+++ readme.md
@@ -0,0 +1,10 @@
+# route and extension
+- order of routes matters.
+- if any url matters than that will hit first.
+
+# http methods
+- post
+- get
+- patch
+- delete
+
--- src/app.js
+++ src/app.js
@@ -2,12 +2,32 @@ const express= require ('express');
 
 const app=express();
 
+app.get("/user",(req, res)=>{
+  res.send({firstName: "deepak"})
+})
+
+app.post("/user", (req, res)=>{
+  res.send("data successfully saved to database")
+})
+
+app.delete("/user", (req, res)=>{
+  res.send("data deleted successfully"); 
+
+})
+app.get("/ab*c", (req, res)=>{
+  res.send({fistname: "Deepak", lastname: "kumar"})
+})
+app.get("/ab+c", (req, res)=>{
+  res.send({fistname: "Deepak", lastname: "kumar"})
+})
+app.get("/ab?c", (req, res)=>{
+  res.send({fistname: "Deepak", lastname: "kumar"})
+})
+
 app.use("/test",(req, res)=>{
   res.send("hello from the server!")
 })
-app.use("/",(req, res)=>{
-  res.send("hello from home page")
-})
+
 
 app.listen(3000, ()=>{
   console.log("server is successfullly listening on port 3000....")
