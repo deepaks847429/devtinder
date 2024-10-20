77c3156c5da91e5992a9e6adca33d704c6215797
ambitious_hustler <deepaks9631342845@gmail.com>
2024-10-20 21:07:01 +0530

    created user schema and created signup api

--- src/app.js
+++ src/app.js
@@ -1,34 +1,30 @@
 const express= require ('express');
-
+const connectDB=require("./config/database");
 const app=express();
+const User=require("./models/user")
 
-app.get("/user",(req, res)=>{
-  res.send({firstName: "deepak"})
-})
-
-app.post("/user", (req, res)=>{
-  res.send("data successfully saved to database")
-})
-
-app.delete("/user", (req, res)=>{
-  res.send("data deleted successfully"); 
-
-})
-app.get("/ab*c", (req, res)=>{
-  res.send({fistname: "Deepak", lastname: "kumar"})
-})
-app.get("/ab+c", (req, res)=>{
-  res.send({fistname: "Deepak", lastname: "kumar"})
-})
-app.get("/ab?c", (req, res)=>{
-  res.send({fistname: "Deepak", lastname: "kumar"})
-})
-
-app.use("/test",(req, res)=>{
-  res.send("hello from the server!")
-})
-
-
-app.listen(3000, ()=>{
-  console.log("server is successfullly listening on port 3000....")
+app.post("/signup", async(req, res)=>{
+  const user = new User({
+    firstName: "Deepak",
+    lastName: "Kumar",
+    emailid: "deep.dkt0@gmail.com",
+    password:"deepak@123"
+  });
+  await user.save();
+  res.send("User added successfully!")
 });
+
+connectDB()
+.then(()=>{
+  console.log("database connected successfully");
+  app.listen(3000, ()=>{
+    console.log("server is successfullly listening on port 3000....")
+  });
+})
+.catch((err)=>{
+
+  console.error("Database cannot be connected");
+});
+
+
+
